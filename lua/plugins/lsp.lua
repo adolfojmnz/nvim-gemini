26835cf6002d2f95094f6fab89b0dd9cf7f02216
Adolfo Jimenez <adolfoj@protonmail.com>
2025-07-16 12:52:16 -0400

    feat(core): Add Treesitter and fix LSP load order
    Add the nvim-treesitter plugin to manage language parsers. This
    resolves startup errors related to missing parsers and provides
    more robust syntax highlighting.
    
    Correct a race condition where mason-lspconfig could initialize
    before mason.nvim was fully loaded. This is fixed by adding
    mason.nvim as an explicit dependency to the lspconfig plugin spec.
    
    Update lazy-lock.json to reflect the plugin changes.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,24 +10,20 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
+      -- This dependency order is crucial.
+      -- It ensures mason is loaded before mason-lspconfig.
+      "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
       "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
-      -- Set up mason-lspconfig
       require("mason-lspconfig").setup({
-        -- A list of servers to automatically install if they're not already installed
         ensure_installed = { "lua_ls", "pyright", "gopls", "rust_analyzer", "tsserver" },
-
-        -- This is the key part: set up a handler that will be called for each server
         handlers = {
-          -- The first entry is the default handler.
-          -- It's fired for every server that is not explicitly handled.
           function(server_name)
             require("lspconfig")[server_name].setup({
               capabilities = require("cmp_nvim_lsp").default_capabilities(),
               on_attach = function(client, bufnr)
-                -- Your on_attach function containing keymaps
                 local keymap = vim.keymap.set
                 local opts = { noremap = true, silent = true, buffer = bufnr }
 
@@ -47,3 +43,4 @@ return {
     end,
   },
 }
+
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+-- ~/.config/nvim/lua/plugins/treesitter.lua
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        -- A list of parser names, or "all"
+        ensure_installed = { "lua", "vim", "vimdoc", "python", "go", "rust", "javascript", "typescript", "json" },
+
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
+
+        -- Automatically install missing parsers when entering buffer
+        auto_install = true,
+
+        highlight = {
+          enable = true,
+        },
+      })
+    end,
+  },
+}
+
